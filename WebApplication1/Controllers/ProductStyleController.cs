17fe34db1e76de2cc8a597cad84082596f81d396
Ruslan <ruslanri@code.edu.az>
2023-05-24 02:11:19 +0400

    BackEnd

---
+++ WebApplication1/Controllers/ProductStyleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ProductStyleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Models/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace WebApplication1.Models
+{
+    public class BaseEntity<T>
+    {
+        public int Id { get; set; }
+    }
+}
