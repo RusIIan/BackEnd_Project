9b310ff8096ebd5a73ac1db0319c3969474659c9
Ruslan <ruslanri@code.edu.az>
2023-06-03 22:55:38 +0400

    BackEnd

--- WebApplication1/Areas/AdminPanel/Controllers/DashboardController.cs
+++ WebApplication1/Areas/AdminPanel/Controllers/DashboardController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebApplication1.Areas.AdminPanel.Controllers
 {
     [Area("AdminPanel")]
+    [Authorize]
     public class DashboardController : Controller
     {
         public IActionResult Index()
