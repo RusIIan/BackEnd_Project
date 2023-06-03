1fdae313062947ada8c5badded80985436a2dcc8
Ruslan <ruslanri@code.edu.az>
2023-06-04 00:11:22 +0400

    BackEnd

--- WebApplication1/Controllers/ShopController.cs
+++ WebApplication1/Controllers/ShopController.cs
@@ -18,23 +18,26 @@ namespace WebApplication1.Controllers
 
         public async Task<IActionResult> Index(int? categoryId,int? colorId,int page=1,int take=5)
         {
-            var query = _context.HomeProducts.Include(x => x.Category).Include(x=>x.ProductColor).AsQueryable();
-
-            if (categoryId.HasValue && categoryId.Value > 0|| colorId.HasValue && colorId.Value > 0)
+            var query = _context.HomeProducts
+                .Include(x => x.Category)
+                .Include(x=>x.ProductColor).AsQueryable();
+            if (categoryId.HasValue && categoryId.Value > 0 || colorId.HasValue && colorId.Value > 0)
             {
                 query = query.Where(m => m.CategoryId == categoryId||m.ProductColorId==colorId);
             }
             var product = await query.ToListAsync();
 
-            var category = await _context.HomeCategories.Include(x=>x.Products).ToListAsync();
-            var color = await _context.Colors.Include(x=>x.Products).ToListAsync();
+            var category = await _context.HomeCategories
+                .Include(x=>x.Products).ToListAsync();
+
+            var color = await _context.Colors
+                .Include(x=>x.Products).ToListAsync();
 
             var baner = await _context.Baners.FirstOrDefaultAsync();
             //pagination start
-            var products= await _context.HomeProducts
-                .Skip((page-1)*take)
+            var products= await _context.HomeProducts.
+                Skip((page-1)*take)
                 .Take(take).ToListAsync();
-
             int pageCount = await GetPageCount(take);
 
             Pagination<Product> pagination = new(products, page, pageCount);
